aec52e95a663c27f2efd72281b53c6bc03c6b3bc
CouetteChauffante <s.jeames@rubika-edu.com>
2026-05-22 18:02:49 +1200

    FadeToBlack + TransitionsPrefab

--- FractureProject/Assets/Scripts/Managers/TransitionManager.cs
+++ FractureProject/Assets/Scripts/Managers/TransitionManager.cs
@@ -7,11 +7,11 @@ public class TransitionManager : MonoBehaviour
     public static TransitionManager instance { get; private set; }
 
     [SerializeField] private Animator cinematicBarsAnimator;
-
-    public bool test;
+    [SerializeField] private Animator fadeToBlackAnimator;
     
     private static readonly int Cinematic = Animator.StringToHash("isCinematic");
-    
+    private static readonly int Black = Animator.StringToHash("isBlack");
+
     private void Awake()
     {
         if (instance != null && instance != this)
@@ -23,18 +23,7 @@ public class TransitionManager : MonoBehaviour
         instance = this;
     }
 
-    private void Update()
-    {
-        if (test)
-        {
-            StartCinematic();
-        }
-        else
-        {
-            StopCinematic();
-        }
-    }
-
+    #region Cinematic Bars
     public void StartCinematic()
     {
         if (!cinematicBarsAnimator) return;
@@ -52,4 +41,23 @@ public class TransitionManager : MonoBehaviour
         
         //Trigger Camera movement (slight zoom-out by default)
     }
+    #endregion
+
+    #region FadeToBlack
+    public void FadeToBlack()
+    {
+        if (!fadeToBlackAnimator) return;
+
+        Player.instance.locked = true;
+        fadeToBlackAnimator.SetBool(Black, true);
+    }
+
+    public void FadeFromBlack()
+    {
+        if (!fadeToBlackAnimator) return;
+        
+        fadeToBlackAnimator.SetBool(Black, false);
+        Player.instance.locked = false;
+    }
+    #endregion
 }
