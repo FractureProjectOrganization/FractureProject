e54d9228def5fa0b51d55ecc3a07ff387b9f7ab2
FrançoisAuCanada <f.dassonville@rubika-edu.com>
2026-06-02 14:06:05 -0400

    Flashback 1 - End Fight Chain Of Events

--- FractureProject/Assets/Scripts/CameraTargetTrigger.cs
+++ FractureProject/Assets/Scripts/CameraTargetTrigger.cs
@@ -53,12 +53,6 @@ public class CameraTargetTrigger : MonoBehaviour
         {
             IsometricCameraFollow.instance.ChangeTarget(savedTarget);
         }
-        
-        FlashbackGuards guardsZone = GetComponentInParent<FlashbackGuards>();
-        if (guardsZone != null)
-        {
-            guardsZone.MarkAsObserved();
-        }
     }
     
     private IEnumerator BlockAndReleasePlayer(GameObject playerObject)
