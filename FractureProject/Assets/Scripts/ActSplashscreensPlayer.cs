3d6619f4c4e1d74afec5a54a8889e6f9f9717d3b
Nikokwak <nicolas.gourdon45@gmail.com>
2026-06-12 20:46:32 -0400

    transitions actes et tout (à teste mais normalement is ok, aussi si on a un problème de lag un jour contactez-moi j'ai écrit un crime dans ces scripts)

---
+++ FractureProject/Assets/Scripts/ActSplashscreensPlayer.cs
@@ -0,0 +1,35 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class ActSplashscreensPlayer : MonoBehaviour
+{
+    private Animator anim;
+    private string save;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+   private void FixedUpdate()
+   {
+       if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name != save)
+       {
+           save = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+           TestTrigger();
+       }
+   }
+
+   private void TestTrigger()
+   {
+       switch (save)
+       {
+           case "Present I" : anim.SetTrigger("Tr_Acte1"); break;
+           case "Present II" : anim.SetTrigger("Tr_Acte2"); break;
+           case "Present III" : anim.SetTrigger("Tr_Acte3"); break;
+           default: break;
+
+       }
+   }
+}
