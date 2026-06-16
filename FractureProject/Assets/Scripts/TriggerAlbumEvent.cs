ecc9e13607fc45f9d81b5d3a92f985e3dee15008
Nikokwak <nicolas.gourdon45@gmail.com>
2026-06-16 16:45:09 -0400

    Dialogues adaptés aux "cinématiques", normalement ca marche

--- FractureProject/Assets/Scripts/TriggerAlbumEvent.cs
+++ FractureProject/Assets/Scripts/TriggerAlbumEvent.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 public class TriggerAlbumEvent : MonoBehaviour
 {
     private Animator anim;
+    private GameObject[] images;
 
     private void Start()
     {
         anim = AlbumPhoto.instance.gameObject.GetComponent<Animator>();
+        images = AlbumPhoto.instance.images;
     }
 
-    public void AlbumShow()
+    public void AlbumShow(int imageIndex = 0)
     {
+        for(int i = 0; i < images.Length; i++)
+        {
+            if(i==imageIndex) images[i].SetActive(true);
+            else images[i].SetActive(false);
+        }
         anim.SetTrigger("Open");
     }
 
